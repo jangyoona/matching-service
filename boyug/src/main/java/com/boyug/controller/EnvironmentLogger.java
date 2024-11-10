40b53c8d53b69a4484ec8166fb732717f1943d89
jangyoona <olozg@naver.com>
2024-11-10 17:42:37 +0900

    Test webHook

--- boyug/src/main/java/com/boyug/controller/EnvironmentLogger.java
+++ boyug/src/main/java/com/boyug/controller/EnvironmentLogger.java
@@ -1,17 +1,17 @@
-package com.boyug.controller;
-
-import jakarta.annotation.PostConstruct;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-@Component
-public class EnvironmentLogger {
-    private static final Logger logger = LoggerFactory.getLogger(EnvironmentLogger.class);
-
-    @PostConstruct
-    public void logEnvironmentVariables() {
-        logger.info("DB URL: {}", System.getenv("DB_URL"));
-        logger.info("DB Username: {}", System.getenv("DB_USERNAME"));
-    }
-}
+//package com.boyug.controller;
+//
+//import jakarta.annotation.PostConstruct;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class EnvironmentLogger {
+//    private static final Logger logger = LoggerFactory.getLogger(EnvironmentLogger.class);
+//
+//    @PostConstruct
+//    public void logEnvironmentVariables() {
+//        logger.info("DB URL: {}", System.getenv("DB_URL"));
+//        logger.info("DB Username: {}", System.getenv("DB_USERNAME"));
+//    }
+//}
