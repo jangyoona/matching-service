e533578728df9828260a67650e27e375621de53c
jangyoona <olozg@naver.com>
2025-01-22 21:38:33 +0900

    Add Senior Information tab To Navbar

--- boyug/src/main/java/com/boyug/BoyugApplication.java
+++ boyug/src/main/java/com/boyug/BoyugApplication.java
@@ -1,6 +1,5 @@
 package com.boyug;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -18,8 +17,8 @@ public class BoyugApplication {
 	public static void main(String[] args) {
 
 		// Spring Environment 객체에 Env 파일 내용 전체 등록
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+//		Dotenv dotenv = Dotenv.configure().load();
+//		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 		// 개별 등록
 		// System.setProperty("DB_URL", dotenv.get("DB_URL"));
